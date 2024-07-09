ce3ab8b746adf9834645946ca2597b99ad865d2e
htmcharles <hatumacharles1@gmail.com>
2024-07-09 08:26:19 +0200

    CRUD with mongoose

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,11 +1,40 @@
 const Movie = require('./../models/movieModel');
 
-exports.getALLmovies = (req, res) => {
-
+exports.getALLmovies = async(req, res) => {
+    try{
+        const movies = await Movie.find()
+        res.status(200).json({
+            status:'SUCCESS',
+            length:movies.length,
+            data:{
+                movies
+            }
+        })
+    }
+    catch(err){
+        res.status(404).json({
+            status:'fail',
+            message:err.message
+        })
+    }
 };
 
-exports.getOneMovie = (req, res) => {
-
+exports.getOneMovie = async(req, res) => {
+    try{
+        const movie = await Movie.findById(req.params.id)
+        res.status(200).json({
+            status:'SUCCESS',
+            data:{
+                movie
+            }
+        })
+    }
+    catch(err){
+        res.status(404).json({
+            status:'fail',
+            message:'Movie not found'
+        })
+    }
 };
 
 exports.createMovie = async(req, res) => {
@@ -18,7 +47,6 @@ exports.createMovie = async(req, res) => {
             }
         })
     }catch(err){
-        console.error('Error creating movie:', err);
         res.status(400).json({
             status:'fail',
             message:err.message
@@ -27,10 +55,36 @@ exports.createMovie = async(req, res) => {
 
 };
 
-exports.updateMovie = (req, res) => {
-    
+exports.updateMovie = async(req, res) => {
+    try{
+        const updateMovie = await Movie.findByIdAndUpdate(req.params.id, req.body,{new:true,runValidators:true})
+        res.status(200).json({
+            status:'SUCCESS',
+            data:{
+                updateMovie
+            }
+        })
+    }
+    catch(err){
+        res.status(404).json({
+            status:'fail',
+            message:err.message
+        })
+    }
 };
 
-exports.deleteMovie = (req, res) => {
-    
+exports.deleteMovie = async(req, res) => {
+    try{
+        const movieDeleted = await Movie.findByIdAndDelete(req.params.id)
+        res.status(204).json({
+            status:'SUCCESS',
+            data:null
+        })
+    }
+    catch(err){
+        res.status(404).json({
+            status:'fail',
+            err:err.message
+        })
+    }
 };
