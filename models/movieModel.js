5b98e6c485fda31ddee614107c6ec68141efb148
htmcharles <hatumacharles1@gmail.com>
2024-07-09 08:36:16 +0200

    Movie schema Modification

--- models/movieModel.js
+++ models/movieModel.js
@@ -4,16 +4,58 @@ const movieSchema = new mongoose.Schema({
     name:{
         type:String,
         required:[true,'Name is a required field'],
-        unique:true
+        unique:true,
+        trim:true
     },
-    description:String,
+    description:{
+        type:String,
+        required:[true,'Description is a required field'],
+        trim:true
+    },
     duration :{
         type:Number,
-        required:[true,'Duration is a required field']
+        required:[true,'Duration is a required field'],
+        trim:true
     },
     ratings:{
+        type:Number
+    },
+    totalRating:{
+        type:Number
+    },
+    releaseYear:{
         type:Number,
-        default:1.0
+        required:[true,'Release year is a required field'],
+        trim:true
+    },
+    releaseDate:{
+        type:Date
+    },
+    createdAT:{
+        type:Date,
+        default:Date.now()
+    },
+    genres:{
+        type:[String],
+        required:[true,'Genres is a required field'],
+        
+    },
+    directors:{
+        type:[String],
+        required:[true,'Directors is a required field'],
+       
+    },
+    coverImage:{
+        type:String,
+        require:[true,"Cover image is required filed"]
+    },
+    actors:{
+        type:String,
+        require:[true,"Actors is required filed"]
+    },
+    price:{
+        type:Number,
+        requre:[true,"Price is a required field"]
     }
 });
 
